7a9cc7fbf1ae7fd3858075c80b3aa70b3063b480
Roberto Matheus <robertmatheusa@gmail.com>
2024-06-26 17:37:17 -0300

    feat: creating repositories

---
+++ src/main/java/com/bookstore/jpa/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.jpa.repositories;
+
+import com.bookstore.jpa.model.BookModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<BookModel, UUID> {
+
+}
