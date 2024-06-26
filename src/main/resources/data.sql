5a0ca51a7d5173d30c403e40df46e5fdb93fdefd
Roberto Matheus <robertmatheusa@gmail.com>
2024-06-26 17:47:47 -0300

    feat: fixing configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into tb_publisher(id,name) values ('7f33f4cb-1412-4c0b-9262-148ef552d944','Publisher1');
+insert into tb_author(id,name) values ('3b8ddddc-ac1a-4bd7-ac4c-51a32f7c52bc','author1');
