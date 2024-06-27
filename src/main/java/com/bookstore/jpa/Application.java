0005c07b3e951349d620c83499e0c5e1688502da
Roberto Matheus <robertmatheusa@gmail.com>
2024-06-26 21:45:59 -0300

    Bugfix: CORS

--- src/main/java/com/bookstore/jpa/Application.java
+++ src/main/java/com/bookstore/jpa/Application.java
@@ -1,9 +1,12 @@
 package com.bookstore.jpa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(url = "/",description = "Default server URL"))
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
